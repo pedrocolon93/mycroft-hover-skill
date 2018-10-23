25db1be0138664ce484754d2acb34711e21616ae
Pedro <pe25171@mit.edu>
2018-10-22 22:14:58 -0400

    Fixed spaces

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ class HoverSkill(MycroftSkill):
             "match_amount":0,
             "info":""
         }
-	    self.db.insert({'classname': "ball",
+        self.db.insert({'classname': "ball",
                         'info': 'This is a collection of metal balls from ball bearings held together'
                                 'with a magnet.'})
 
@@ -53,25 +53,25 @@ class HoverSkill(MycroftSkill):
                        'info': 'This is a pine cone from a Sugar Pine.  The worlds largest breed of'
                                'pine tree.'})
         # Add spectrometer
-        self.db.insert({'classname': "spectrometer01",
+        self.db.insert({'classname': "spectrometer",
                    'info': 'This is a Tektronix 1L20 spectrum analyzer plug-in'})
         # Add stanley
-        self.db.insert({'classname': "stanley01",
+        self.db.insert({'classname': "stanley",
                    'info': 'This is a Stanley 45 combination plane. It is a woodworking '
                            'tool used to make fine fittings such as those used for doors.'})
         # Add visual rejects box
-        self.db.insert({'classname': "visualrejects01",
+        self.db.insert({'classname': "visualrejects",
                    'info': 'This is a box from a gold mine in Black Hawk city in Colorado that was used to hold '
                            'the samples that needed further inspection.'})
 
         # Add visual rejects box
-        self.db.insert({'classname': "washing01",
+        self.db.insert({'classname': "washing",
                    'info': 'This is an model of an early washing machine patent. In the 1800s, models were required when '
                            'submitting for a patent in the US.'})
         #
         self.db.insert({'classname': "snacks",
                         'info': 'This is a box that had complementary snacks.'})
-        self.db.insert({'classname': "pusheen01",
+        self.db.insert({'classname': "pusheen",
                         'info': 'A fat cute cat in an easter egg.'})
         LOG.info("Working in...")
         LOG.info(os.path.abspath("."))
